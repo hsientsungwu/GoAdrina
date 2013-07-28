dc0673b885df08a73d6830a4ce48b7c174d686d7
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-07-28 16:59:23 -0400

    Add Foundation

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <html>
-	<head></head>
+	<head>
+		<title>Go Andrina!</title>
+		<link rel="stylesheet" href="css/normalize.css" />
+  		<link rel="stylesheet" href="css/foundation.css" />
+	</head>
 	<body>
-		Testing
-		
+		<form>
+  			<div class="row">
+    			<div class="small-8">
+      				<div class="row">
+        				<div class="small-3 columns">
+          					<label for="right-label" class="right inline">Label</label>
+        				</div>
+        				<div class="small-9 columns">
+          					<input type="text" id="right-label" placeholder="Inline Text Input">
+        				</div>
+      				</div>
+    			</div>
+  			</div>
+		</form>
 	</body>
 </html>

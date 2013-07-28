e6687424809b41787b8615466c5b6dd4b625d7e6
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-07-28 18:35:46 -0400

    displaying results and pagination

--- search/index.php
+++ search/index.php
@@ -4,8 +4,21 @@ require($_SERVER['DOCUMENT_ROOT'] . '/config.php');
 
 if ($_GET) {
 	$key = htmlentities($_GET['k']);
+	$page = ($_GET['page'] ? $_GET['page'] : 1);
 
-	$results = search($key);
+	$results = search($key, $page);
+
+	if ($results['total'] < $results['limit']) {
+		$pages = 1;
+	} else {
+		$pages = ceil($results['total'] / $results['limit']);
+	}
+	
+	if ($results['page'] == 0) {
+		$current = 1;
+	} else {
+		$current = $results['page'];
+	}
 }
 
 ?>
@@ -21,18 +34,42 @@ if ($_GET) {
 	<body>
 		<?php include($_SERVER['DOCUMENT_ROOT'] . "/frontend/searchbar.frontend.php"); ?>
 
-		<?php
-
-		if (count($results)) {
-
-		} else { ?>
+		<div class="results-container" style="min-height: 500px;">
 			<div class="row">
-				<div class="small-8 large-centered columns">
+				<div class="small-12 large-centered columns">
+			<?php
+
+			if (count($results['data'])) {
+				foreach ($results['data'] as $index => $result) { ?>
+					<p>
+						<span>
+							<?php 
+								echo date('M j', strtotime($result['created_time'])) . 
+								' - <a href="' . $result['link'] . '" target="_blank">' . $result['message'] . '</a> ' . 
+								($result['thumbnail'] ? '<font color="green"><i>pic</i></font>' : ''); 
+							?>
+						</span>
+					</p>
+				<?php }
+			} else { ?>
 					<p>No result found ... </p>
-				</div>
-			</div>	
+			<?php } ?>
+					</div>
+			</div>
 
-
-		<?php } ?>
+			<div class="pagination-centered">
+			  	<ul class="pagination">
+			  		<li class="arrow <?php echo ($current <= 1 ? 'unavailable' : ''); ?>"><a href="/search/?k='<?php echo $_GET['key']; ?>&page=<?php echo ($current-1 >= 1 ? $current-1 : $current); ?>">&laquo;</a></li>
+			  		<?php
+			  			for($i=1; $i <= $pages; $i++) {
+			  				$class = ($i == $current ? 'current unavailable' : '');
+			  				echo '<li class="' . $class . ' "><a href="/search/?k=' . $_GET['key'] . '&page=' . $i . ' ">' . $i . '</a></li>';
+			  			}
+			  		?>
+				    
+				    <li class="arrow <?php echo ($current >= $page ? 'unavailable' : ''); ?>"><a href="/search/?k=<?php echo $_GET['key']; ?>&page=<?php echo ($current+1 < $pages ? $current+1 : $current); ?>">&raquo;</a></li>
+			  	</ul>
+			</div>
+		</div>
 	</body>
 </html>
--- support/search.func.php
+++ support/search.func.php
@@ -1,5 +1,24 @@
 <?php
 
-function search($key) {
-	return array();
+function search($key, $page = 0) {
+	global $db;
+
+	$limit = 20;
+
+	$data = $db->fetchRows("SELECT message, thumbnail, link, created_time, source FROM facebook_posts WHERE message LIKE '%{$key}%' ORDER BY created_time DESC");
+
+	if (count($data) > $limit) {
+		$result = array_slice($data, $page*$limit, $limit);
+	} else {
+		$result = $data;
+	}
+
+	$returnData = array(
+		'data' => (count($result) ? $result : array()),
+		'total' => count($data),
+		'limit' => $limit,
+		'page' => $page
+	);
+
+	return $returnData;
 }

a0c2125eedc996924dd5e2590a04bc80e4fd68d6
Steve Wu <wu.steve@qgiv.com>
2013-07-29 13:26:54 -0400

    Add Belize Classifieds Facebook Group

--- admin/cron/facebook.cron.php
+++ admin/cron/facebook.cron.php
@@ -12,7 +12,8 @@ $fb_groups = array(
     'Belize Buy & Sell' => '127267637407455',
     'Belize Sell & Trade' => '203507416440736',
     'Cayo Classifield' => '300373796698711',
-    'Area 501 Classifieds and Adds' => '520309021343790'
+    'Area 501 Classifieds and Adds' => '520309021343790',
+    'Belize Classifieds ' => '392248577488534'
 );
 
 $total_posts_count = 0;

493be585b1feab2fef5eeda4f6798e951e5e736a
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-07-30 00:09:14 -0400

    updates

--- admin/posts.php
+++ admin/posts.php
@@ -2,17 +2,4 @@
 ?>
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/admin/frontend/header.frontend.php'); ?>
 
-<form>
-  	<fieldset>
-	    <legend>Fieldset</legend>
-
-	    <div class="row">
-	      <div class="large-6 columns">
-	        <label>Input Label</label>
-	        <input type="text" placeholder="large-12.columns">
-	      </div>
-	    </div>
-    </fieldset>
-</form>
-
 <?php include($_SERVER['DOCUMENT_ROOT'] . '/admin/frontend/footer.frontend.php'); ?>

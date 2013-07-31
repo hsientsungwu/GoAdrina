e2649bece804c1af5be538ee6ebd92a7b2512981
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-07-31 00:36:25 -0400

    api call for google extension

---
+++ api/index.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+require($_SERVER['DOCUMENT_ROOT'] . '/config.php');
+
+if ($_GET) {
+	$key = htmlentities($_GET['k']);
+	$page = ($_GET['page'] ? $_GET['page'] : 1);
+
+	$results = searchAll($key);
+}
+
+if (count($results)) {
+
+	foreach ($results as $index => $result) {
+
+		$thumbnail = (strlen($result['thumbnail']) > 1 ? $result['thumbnail'] : 'nophoto.jpg');
+
+		echo "<div class=\"large-12 columns\"><a target=\"_blank\" href=\"" . $result['link'] . "\"><div class=\"panel text-center post-box\">
+			<span class=\"label\">" . substr($result['message'],0, 30) . "..." . "</span>
+			<img class=\"post-box-img\"src=\"" . $thumbnail . "\" /></div></a></div>";
+
+	}
+} else {
+	echo '<div class="large-12 columns><div class="panel">No result found ... </div></div>';
+}
+

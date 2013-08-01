1848bef094ecbfcb4ba20864278d2b23b7bb2efc
HsienTsung Wu <hsientsungwu@HsienTsungs-MacBook-Pro.local>
2013-08-01 00:38:06 -0400

    test case

--- cron/active/getToTop.cron.php
+++ cron/active/getToTop.cron.php
@@ -8,16 +8,17 @@ global $fb, $db;
 
 $token = getAccessTokenForAdminUser();
 $fb->setAccessToken($token);
-
+print_r('11');
 $postIds = $db->fetchAll("SELECT entityId FROM facebook_entity WHERE entityType = ? AND category = ?", array(FacebookEntityType::POST, AdrinaCategory::FACEBOOKADS));
 
 $totalCount = count($postIds);
 $successCount = 0;
 
 foreach ($postIds as $postId) {
+	print_r('18');
 	$postInfo = $fb->api('/' . $postId['entityId'], 'GET');
-
-	echo "<pre>"; print_r($postInfo); echo "<pre>";
+	print_r('20');
+	//echo "<pre>"; print_r($postInfo); echo "<pre>";
 
 	if (count($postInfo) && $postInfo['id']) {
 
@@ -26,15 +27,18 @@ foreach ($postIds as $postId) {
 		);
 
 		try {
+			print_r('30');
 			$newCommentData = $fb->api('/'. $postId['entityId'] . "/comments", 'POST', $data);
-
+			print_r('32');
 			if (isset($newCommentData['id'])) {
 				sleep(3);
+				print_r('35');
 				$result = $fb->api($newCommentData['id'], 'DELETE');
 				$successCount++;
 			}
 		} catch (FacebookApiException $e) {
-			var_dump($e->getResult());
+			print_r('40');
+			echo "<pre>"; print_r($e->getResult()); echo "<pre>";
 
 			$content['message'] = $e->getResult();
 			$content['type'] = "Facebook Post Ads: {$postId['entityId']}";
@@ -43,7 +47,7 @@ foreach ($postIds as $postId) {
 		}
 	}
 }
-
+print_r('50');
 $emailContent['subject'] = "[Go Adrina!] Facebook Post Ads Cron Result";
 $emailContent['body'] = "Total Post Ads: {$totalCount} and Total SUCCESS: {$successCount}";
 send_email($emailContent, 'admin');
